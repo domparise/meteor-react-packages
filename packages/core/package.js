85b69ed7833086c92ca780bde4f41c92bc517258
domparise <domparise@gmail.com>
2016-03-15 01:33:35 -0400

    working on loading files in package.js

--- packages/core/package.js
+++ packages/core/package.js
@@ -4,27 +4,77 @@ Package.describe({
   version: '0.0.1'
 });
 
-Package.onUse(function(api) {
-
+function addPackagesAndVersion (api) {
   api.versionsFrom("METEOR@1.0");
-  
   var packages = [
     'react-packages:lib@0.0.1',
   ];
-
   api.use(packages);
-  
   api.imply(packages);
+};
 
+function addSharedFiles (api) {
   api.addFiles([
-    'lib/router/config.js',
-  ], ['client', 'server']);
+  ], ['client', 'server']); 
+}
 
+function addClientFiles (api) {
+  var path = 'lib/client',
+  routes = path+'/routes',
+  modules = path+'/modules',
+  components = path+'/components';
   api.addFiles([
-    'lib/client/*'
+    modules+'/_modules.js',
+    modules+'/login.js',
+    modules+'/recover-password.js',
+    modules+'/reset-passworf.js',
+    modules+'/signup.js',
+    modules+'/startup.js',
+    components+'/authenticated/dashboard.jsx',
+    components+'/authenticated/settings.jsx',
+    components+'/globals/authenticated-navigation.jsx',
+    components+'/globals/header.jsx',
+    components+'/globals/loading.jsx',
+    components+'/globals/not-found.jsx',
+    components+'/globals/public-navigation.jsx',
+    components+'/layouts/default.jsx',
+    components+'/modals/index.jsx',
+    components+'/public/login.jsx',
+    components+'/public/recover-password.jsx',
+    components+'/public/reset-password.jsx',
+    components+'/public/signup.jsx',
+    routes+'/configure.jsx',
+    routes+'/public.jsx',
+    routes+'/authenticated.jsx',
   ], 'client');
+};
 
-  api.addFiles([
-    'lib/server/*'
+function addServerFiles (api) {
+    var path = 'lib/server',
+    modules = path+'/modules';
+    api.addFiles([
+    path+'/admin/reset-password.js',
+    modules+'/_modules.js',
+    modules+'/generate-accounts.js',
+    modules+'/set-environment-variables.js',
+    path+'/startup.js',
   ], 'server');
+};
+
+function addFiles (api) {
+  addPackagesAndVersion(api);
+  addClientFiles(api);
+  addServerFiles(api);  
+};
+
+Package.onUse(function (api) {
+
+  addFiles(api);
+
+});
+
+Package.onTest(function (api) {
+
+  addFiles(api);
+
 });
